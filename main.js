b92055058efdd2c8e1ee69b68c2161ae983b5225
DesertWolf <jgfields@gmail.com>
2014-08-12 19:47:54 -0700

    added delete function min.'s and delete button to clientGrid.html

--- main.js
+++ main.js
@@ -34,3 +34,26 @@ function showClients(Clients){
     $(".clientListing").append (theTemplate(Clients));
 
 }
+
+//delete function
+
+$(document).on("click", "#del", function () {
+    var ClientInfo = Parse.Object.extend("ClientGeolocation");
+
+    var delObject = $(this).attr("value");
+
+    var query = new Parse.Query(ClientInfo);
+    query.get(delObject, {
+        success: function (delObj) {
+            // The object was retrieved successfully.
+            delObj.destroy({});
+            window.location = "clientGrid.html";
+        },
+        error: function (object, error) {
+            // The object was not retrieved successfully.
+            // error is a Parse.Error with an error code and description.
+            alert("Error: " + error.code + " " + error.message);
+        }
+    });
+
+});
